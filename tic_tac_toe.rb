0903ef00465574589bcb909f5579a20c49278eee
Timothy <tim87henry@gmail.com>
2020-06-26 23:34:49 +1000

    Added player input validation

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,6 +3,7 @@ class TicTacToe
         @board=[1,2,3,4,5,6,7,8,9]
         @CHOICE=["X","O"]
         @current_player=1
+        @used_keys=[]
         puts "\nPlayer 1 is X and Player 2 is O"
         puts "\nLet the game begin!!"
     end
@@ -21,25 +22,29 @@ class TicTacToe
         while game_not_over? do
             puts "Player #{@current_player} enter your choice : "
             choice=gets.chomp.to_i
+            if choice < 1 or choice > 9 or @used_keys.include?choice
+                puts "Please enter a valid number (0 to 9) : "
+                next
+            end
             play_turn(@current_player,choice)
             draw_board
         end
     end
 
     def play_turn(player,choice)
-        puts @CHOICE[player-1]
         @board[choice-1]=@CHOICE[player-1]
+        @used_keys.push(choice)
         if @current_player == 1 then @current_player=2 else @current_player=1 end 
     end
     
     def game_not_over?
-        if @board.grep(Integer).count()==0
-            puts "It's a tie"
-            false
-        elsif (@board[0]=="X" and @board[1]=="X" and @board[2]=="X") || (@board[3]=="X" and @board[4]=="X" and @board[5]=="X") || (@board[6]=="X" and @board[7]=="X" and @board[8]=="X")
+        if (@board[0]=="X" and @board[1]=="X" and @board[2]=="X") || (@board[3]=="X" and @board[4]=="X" and @board[5]=="X") || (@board[6]=="X" and @board[7]=="X" and @board[8]=="X")
             puts "Player 1 wins"
             false
         elsif (@board[0]=="X" and @board[3]=="X" and @board[6]=="X") || (@board[1]=="X" and @board[4]=="X" and @board[7]=="X") || (@board[2]=="X" and @board[5]=="X" and @board[8]=="X")
+            puts "Player 1 wins"
+            false
+        elsif (@board[0]=="X" and @board[4]=="X" and @board[8]=="X") || (@board[2]=="X" and @board[4]=="X" and @board[6]=="X")
             puts "Player 1 wins"
             false
         elsif (@board[0]=="O" and @board[1]=="O" and @board[2]=="O") || (@board[3]=="O" and @board[4]=="O" and @board[5]=="O") || (@board[6]=="O" and @board[7]=="O" and @board[8]=="O")
@@ -47,6 +52,12 @@ class TicTacToe
             false
         elsif (@board[0]=="O" and @board[3]=="O" and @board[6]=="O") || (@board[1]=="O" and @board[4]=="O" and @board[7]=="O") || (@board[2]=="O" and @board[5]=="O" and @board[8]=="O")
             puts "Player 2 wins"
+            false
+        elsif (@board[0]=="O" and @board[4]=="O" and @board[8]=="O") || (@board[2]=="O" and @board[4]=="O" and @board[6]=="O")
+            puts "Player 2 wins"
+            false
+        elsif @board.grep(Integer).count()==0
+            puts "It's a tie"
             false
         else
             true
